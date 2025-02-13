abbf70e734069ec8264186194e04cb0581ce1bd5
FuwaFuwinski <182450639+FuwinskiFuwa@users.noreply.github.com>
2025-02-14 01:36:59 +0300

    Added syntax-checker copy modified for github actions

---
+++ bash-script/syntax-checker-ga.sh
@@ -0,0 +1,9 @@
+SCRIPT_FILE="service-availability-script.sh"
+bash -n "$SCRIPT_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "Синтаксис скрипта $SCRIPT_FILE в порядке."
+else
+    echo "Ошибка: Синтаксис скрипта $SCRIPT_FILE содержит ошибки или файл не найден."
+    exit 1
+fi
